e73fcec577a86048f15c4c88acaf8bbf75fdfdd9
Jacob Rodriguez <96326236+JRod0409@users.noreply.github.com>
2025-04-30 00:04:25 -0400

    Delete JR_Midterm/top_songs_site directory

--- JR_Midterm/top_songs_site/top_songs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TopSongsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'top_songs'
--- JR_Midterm/top_songs_site/top_songs/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import songs_list
-
-urlpatterns = [
-    path("", songs_list, name='songs_list')
-]
