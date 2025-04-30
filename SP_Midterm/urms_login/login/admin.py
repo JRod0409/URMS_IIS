9e7c938d82b3cb3fd3f9ae703d09d6c7b9d34424
Jacob Rodriguez <96326236+JRod0409@users.noreply.github.com>
2025-04-30 00:04:43 -0400

    Delete SP_Midterm/urms_login directory

--- SP_Midterm/urms_login/login/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User,Admin,Song,TestModel
-
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(Song)
-admin.site.register(TestModel)
